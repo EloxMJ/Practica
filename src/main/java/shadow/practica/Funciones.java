1a3b2437b1e7466e4969c54bbc08c400ce22f749
EloxMJ <kenny.zrky@gmail.com>
2026-01-02 22:57:56 -0600

    Se agregaron métodos de absoluto y potencia

--- src/main/java/shadow/practica/Funciones.java
+++ src/main/java/shadow/practica/Funciones.java
@@ -9,21 +9,41 @@ package shadow.practica;
  * @author diana
  */
 public class Funciones {
-    public String EsPositivo(int _numero){
-        if (_numero>0){
+    public String EsPositivo(int _numero) {
+        if (_numero > 0) {
             return "Es positivo";
-        }else if (_numero<0){
+        } else if (_numero<0) {
             return "Es Negativo";
-         }else{
+        } else {
             return "Es cero";
         }
     }
     
-    public String EsParONon(int _numero){
-        if (_numero%2 == 0 ){
+    public String EsParONon(int _numero) {
+        if (_numero % 2 == 0 ) {
             return "Es par";
-        }else{
+        } else{
             return "Es Non";
         }
+    }
+    
+    public double Cuadrado(int _numero) {
+        return Math.pow(_numero, 2);
+    }
+    
+    public double Cubo(int _numero) {
+        return Math.pow(_numero, 3);
+    }
+    
+    public double Absoluto(int _numero) {
+        return Math.abs(_numero);
+    }
+    
+    public double RaizCuadrada(int _numero) {
+        return Math.sqrt(_numero);
+    }
+    
+    public double RaizCubica(int _numero) {
+        return Math.cbrt(_numero);
     }
 }
